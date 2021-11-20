df45f73efbabd8a4d677ac5054fe85cd929b745d
Raymond Morris <raymondmorris80@gmail.com>
2021-11-19 19:48:16 -0700

    second commit

--- routes/assets.js
+++ routes/assets.js
@@ -2,9 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const fetch = (url) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(url));
+import("node-fetch").then(({ default: fetch }) => fetch(url));
 let [response, data] = [null, null];
-const Assets = require("../models/Assets.js");
 const SALE_COUNT_URL =
   "https://api.opensea.io/api/v1/assets?order_by=sale_count&order_direction=desc&offset=0&limit=20";
 
